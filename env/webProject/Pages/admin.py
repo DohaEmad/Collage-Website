08ce297499ad96dda6c036e03ec19d9b3fa1307d
DohaEmad <102623555+DohaEmad@users.noreply.github.com>
2022-10-25 22:43:17 +0200

    doha

--- env/webProject/Pages/admin.py
+++
@@ -1,6 +0,0 @@
-from Pages.models import Student
-from django.contrib import admin
-
-# Register your models here.
-
-admin.site.register(Student)
--- env/webProject/Users/urls.py
+++
@@ -1,9 +0,0 @@
-from re import template
-from django.urls import path
-from django.contrib.auth import views as loginn
-from . import views
-urlpatterns=[
-   # path('login/', loginn.LoginView.as_view(template_name='pages/login.html'), name='login'),
-    path('login/',views.login,name='login')
-
-]
